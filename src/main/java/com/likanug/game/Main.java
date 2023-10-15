7d3c396180cf8fa127db29abf542fb3bf8cf24ad
likanu-g <1452930742@qq.com>
2023-10-15 17:25:32 +0800

    项目的初始化配置，可以显示桌面窗口

--- src/main/java/com/likanug/game/Main.java
+++ src/main/java/com/likanug/game/Main.java
@@ -1,7 +1,43 @@
 package com.likanug.game;
 
-public class Main {
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+
+    public static int WIDTH = 800;
+    public static int HEIGHT = 600;
+
+    public static final int FPS = 60;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PApplet.main("com.likanug.game.Main");
+    }
+
+    @Override
+    public void settings() {
+        size(WIDTH, HEIGHT);
+    }
+
+
+    @Override
+    public void setup() {
+        frameRate(FPS);
+    }
+
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public void mouseClicked() {
+        super.mouseClicked();
+    }
+
+    @Override
+    public void keyPressed() {
+        super.keyPressed();
     }
 }
